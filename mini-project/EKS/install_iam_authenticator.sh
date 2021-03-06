3d282438e6b7369ab618e8a0129e8c5e05129d4c
ANIL <aaa@ncodeit.com>
2021-03-06 20:31:59 +0530

    install-iam-authenticator script added

---
+++ mini-project/EKS/install_iam_authenticator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# inspired from https://docs.aws.amazon.com/eks/latest/userguide/install-aws-iam-authenticator.html
+# Author : Anil Kintala
+
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator.sha256
+openssl sha1 -sha256 aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+cat <<EOCAT
+-- QUICK LIST OF COMMANDS --
+aws-iam-authenticator help
+EOCAT
